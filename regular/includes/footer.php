999bc16294ae23ae7aed5462ce1554feb51addc7
JohnAngeloJunio <johnangelojunio.hs@gmail.com>
2024-08-02 01:47:04 +0800

    Revised index.php navbar, user side footer

--- regular/includes/footer.php
+++ regular/includes/footer.php
@@ -57,19 +57,20 @@
                             Contact
                         </p>
                     </div>
-                    <div class="d-flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#527853" class="bi bi-envelope-fill ms-5" viewBox="0 0 16 16">
-                            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
-                        </svg>
-                        <p class="mb-1 ms-2 text-white">Email</p>
+                    <div class="float-sm-end">
+                        <div class="d-flex justify-content-center justify-content-sm-start">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#527853" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+                            </svg>
+                            <p class="mb-1 ms-2 text-white">Email</p>
+                        </div>
+                        <div class="d-flex justify-content-center justify-content-sm-start">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#527853" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                            </svg>
+                            <p class="mb-1 ms-2 text-white">Contact Number</p>
+                        </div>
                     </div>
-                    <div class="d-flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#527853" class="bi bi-telephone-fill ms-5" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
-                        </svg>
-                        <p class="mb-1 ms-2 text-white">Contact Number</p>
-                    </div>
-                    <br>
                 </div>
                 <div class="col-sm-6">
                     <div class="d-flex justify-content-center">
@@ -77,17 +78,19 @@
                             Links
                         </p>
                     </div>
-                    <div class="d-flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#527853" class="bi bi-envelope" viewBox="0 0 16 16">
-                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-                        </svg>
-                        <p class="mb-1 ms-2 text-white">Email</p>
-                    </div>
-                    <div class="d-flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#527853" class="bi bi-telephone-fill" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
-                        </svg>
-                        <p class="mb-1 ms-2 text-white">Contact Number</p>
+                    <div class="float-sm-start">
+                        <div class="d-flex justify-content-center justify-content-sm-start">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#527853" class="bi bi-envelope" viewBox="0 0 16 16">
+                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                            </svg>
+                            <p class="mb-1 ms-2 text-white">Email</p>
+                        </div>
+                        <div class="d-flex justify-content-center justify-content-sm-start">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#527853" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                            </svg>
+                            <p class="mb-1 ms-2 text-white">Contact Number</p>
+                        </div>
                     </div>
                 </div>
                 <div class="col-12 mt-3">
