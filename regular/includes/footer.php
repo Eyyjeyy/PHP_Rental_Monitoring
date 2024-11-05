eb9ed1c0046346bbaefb9a039a1b72298debb24b
JohnAngeloJunio <johnangelojunio.hs@gmail.com>
2024-11-05 17:55:58 +0800

    user side footer links fix

--- regular/includes/footer.php
+++ regular/includes/footer.php
@@ -146,7 +146,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"" class="bi bi-telephone-fill phone-icon" viewBox="0 0 16 16">
                         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
                         </svg>
-                        <a class="mb-2" href="chat_user.php" id="linklink">Info</a>
+                        <a class="mb-2" href="<?php echo (($pageTitle ?? '') != 'Payments Page') ? 'info.php' : '../info.php'; ?>" id="linklink">Info</a>
                     </div>
     </div>
     <div class="hover-wrapper">
@@ -154,7 +154,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="bi bi-telephone-fill phone-icon" viewBox="0 0 16 16">
                         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
                         </svg>
-                        <a class="mb-2" href="chat_user.php" id="linklink">Profile</a>
+                        <a class="mb-2" href="<?php echo (($pageTitle ?? '') != 'Payments Page') ? 'profile_user.php' : '../profile_user.php'; ?>" id="linklink">Profile</a>
                     </div>
     </div>
     <div class="hover-wrapper">
@@ -162,7 +162,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="bi bi-telephone-fill phone-icon" viewBox="0 0 16 16">
                         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
                         </svg>
-                        <a class="mb-1" href="chat_user.php" id="linklink">Chat</a>
+                        <a class="mb-1" href="<?php echo (($pageTitle ?? '') != 'Payments Page') ? 'chat_user.php' : '../chat_user.php'; ?>" id="linklink">Chat</a>
                     </div>
     </div>
                 </div>
